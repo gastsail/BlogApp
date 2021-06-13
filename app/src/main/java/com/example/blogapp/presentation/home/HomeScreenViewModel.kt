3b8b884ebb87805f68c7d1cc8adf9f631d3d8dcc
Gastón Saillén <gsaillen95@gmail.com>
2021-06-13 17:07:08 -0300

    StateFlow demo

--- app/src/main/java/com/example/blogapp/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/blogapp/presentation/home/HomeScreenViewModel.kt
@@ -3,9 +3,15 @@ package com.example.blogapp.presentation.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.blogapp.core.Result
+import com.example.blogapp.data.model.Post
 import com.example.blogapp.domain.home.HomeScreenRepo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import java.lang.Exception
 
 class HomeScreenViewModel(private val repo: HomeScreenRepo): ViewModel() {
@@ -18,6 +24,16 @@ class HomeScreenViewModel(private val repo: HomeScreenRepo): ViewModel() {
             emit(Result.Failure(e))
         }
     }
+
+
+    // with Flow coroutine builder
+    val latestPosts: StateFlow<Result<List<Post>>> = flow {
+        emit(repo.getLatestPosts())
+    }.stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5000), // Or Lazily because it's a one-shot
+            initialValue = Result.Loading()
+    )
 }
 
 
--- app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/example/blogapp/ui/home/HomeScreenFragment.kt
@@ -5,7 +5,10 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.blogapp.R
 import com.example.blogapp.core.Result
 import com.example.blogapp.core.hide
@@ -16,6 +19,8 @@ import com.example.blogapp.domain.home.HomeScreenRepoImpl
 import com.example.blogapp.presentation.home.HomeScreenViewModel
 import com.example.blogapp.presentation.home.HomeScreenViewModelFactory
 import com.example.blogapp.ui.home.adapter.HomeScreenAdapter
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
@@ -32,32 +37,36 @@ class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeScreenBinding.bind(view)
 
-        viewModel.fetchLatestPosts().observe(viewLifecycleOwner, Observer { result ->
-            when (result) {
-                is Result.Loading -> {
-                    binding.progressBar.show()
-                }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.latestPosts.collect { result ->
+                    when (result) {
+                        is Result.Loading -> {
+                            binding.progressBar.show()
+                        }
 
-                is Result.Success -> {
-                    binding.progressBar.hide()
-                    if(result.data.isEmpty()) {
-                        binding.emptyContainer.show()
-                        return@Observer
-                    }else{
-                        binding.emptyContainer.hide()
+                        is Result.Success -> {
+                            binding.progressBar.hide()
+                            if(result.data.isEmpty()) {
+                                binding.emptyContainer.show()
+                                return@collect
+                            }else{
+                                binding.emptyContainer.hide()
+                            }
+                            binding.rvHome.adapter = HomeScreenAdapter(result.data)
+                        }
+
+                        is Result.Failure -> {
+                            binding.progressBar.hide()
+                            Toast.makeText(
+                                requireContext(),
+                                "Ocurrio un error: ${result.exception}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
-                    binding.rvHome.adapter = HomeScreenAdapter(result.data)
-                }
-
-                is Result.Failure -> {
-                    binding.progressBar.hide()
-                    Toast.makeText(
-                            requireContext(),
-                            "Ocurrio un error: ${result.exception}",
-                            Toast.LENGTH_SHORT
-                    ).show()
                 }
             }
-        })
+        }
     }
 }
