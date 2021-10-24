b3e5a194a45d88ac3c8b358c398fa833ff3e2b9c
Gastón Saillén <gsaillen95@gmail.com>
2021-10-24 12:16:35 -0300

    Moved view logic inside viewholder

--- app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/example/blogapp/ui/home/adapter/HomeScreenAdapter.kt
@@ -47,65 +47,64 @@ class HomeScreenAdapter(private val onPostClickListener: OnPostClickListener) :
         val context: Context
     ) : BaseViewHolder<Post>(binding.root) {
         override fun bind(item: Post) {
-            setupProfileInfo(context, item, binding)
-            addPostTimeStamp(item, binding)
-            setupPostImage(context, item, binding)
-            setupPostDescription(item, binding)
-            tintHearthIcon(binding, !item.liked, context)
-            setupLikeCount(item, binding)
-            setLikeClickAction(context, item, binding)
+            setupProfileInfo(item)
+            addPostTimeStamp(item)
+            setupPostImage(item)
+            setupPostDescription(item)
+            tintHearthIcon()
+            setupLikeCount(item)
+            setLikeClickAction(item)
         }
-    }
 
-    private fun setupProfileInfo(context: Context, item: Post, binding: PostItemViewBinding) {
-        Glide.with(context).load(item.profile_picture).centerCrop().into(binding.profilePicture)
-        binding.profileName.text = item.profile_name
-    }
-
-    private fun addPostTimeStamp(item: Post, binding: PostItemViewBinding) {
-        val createdAt = (item.created_at?.time?.div(1000L))?.let {
-            TimeUtils.getTimeAgo(it.toInt())
+        private fun setupProfileInfo(item: Post) {
+            Glide.with(context).load(item.profile_picture).centerCrop().into(binding.profilePicture)
+            binding.profileName.text = item.profile_name
         }
-        binding.postTimestamp.text = createdAt
-    }
 
-    private fun setupPostImage(context: Context, item: Post, binding: PostItemViewBinding) {
-        Glide.with(context).load(item.post_image).centerCrop().into(binding.postImage)
-    }
-
-    private fun setupPostDescription(item: Post, binding: PostItemViewBinding) {
-        if (item.post_description.isEmpty()) {
-            binding.postDescription.visibility = View.GONE
-        } else {
-            binding.postDescription.text = item.post_description
+        private fun addPostTimeStamp(item: Post) {
+            val createdAt = (item.created_at?.time?.div(1000L))?.let {
+                TimeUtils.getTimeAgo(it.toInt())
+            }
+            binding.postTimestamp.text = createdAt
         }
-    }
 
-    private fun tintHearthIcon(binding: PostItemViewBinding, liked: Boolean, context: Context) {
-        if(!liked) {
-            binding.likeBtn.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_outline_heart))
-            binding.likeBtn.setColorFilter(ContextCompat.getColor(context, R.color.black))
-        } else {
-            binding.likeBtn.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_filled_heart))
-            binding.likeBtn.setColorFilter(ContextCompat.getColor(context, R.color.red_like))
+        private fun setupPostImage(item: Post) {
+            Glide.with(context).load(item.post_image).centerCrop().into(binding.postImage)
         }
-    }
 
-    private fun setupLikeCount(item: Post, binding: PostItemViewBinding) {
-        if(item.likes > 0) {
-            binding.likeCount.visibility = View.VISIBLE
-            binding.likeCount.text = "${item.likes} likes"
-        } else {
-            binding.likeCount.visibility = View.GONE
+        private fun setupPostDescription(item: Post) {
+            if (item.post_description.isEmpty()) {
+                binding.postDescription.visibility = View.GONE
+            } else {
+                binding.postDescription.text = item.post_description
+            }
         }
-    }
 
-    private fun setLikeClickAction(context: Context, item: Post, binding: PostItemViewBinding) {
-        binding.likeBtn.setOnClickListener {
-            liked = !liked
-            item.apply {  }
-            tintHearthIcon(binding, liked, context)
-            postClickListener?.onLikeButtonClick(item, liked)
+        private fun tintHearthIcon() {
+            if(!liked) {
+                binding.likeBtn.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_outline_heart))
+                binding.likeBtn.setColorFilter(ContextCompat.getColor(context, R.color.black))
+            } else {
+                binding.likeBtn.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_filled_heart))
+                binding.likeBtn.setColorFilter(ContextCompat.getColor(context, R.color.red_like))
+            }
+        }
+
+        private fun setupLikeCount(item: Post) {
+            if(item.likes > 0) {
+                binding.likeCount.visibility = View.VISIBLE
+                binding.likeCount.text = "${item.likes} likes"
+            } else {
+                binding.likeCount.visibility = View.GONE
+            }
+        }
+
+        private fun setLikeClickAction(item: Post) {
+            binding.likeBtn.setOnClickListener {
+                liked = !liked
+                tintHearthIcon()
+                postClickListener?.onLikeButtonClick(item, liked)
+            }
         }
     }
 }
